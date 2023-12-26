2b3b1c4dea8c4114d101d655908bc32e5585d69c
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2023-12-26 15:02:35 +0000

    radius

---
+++ part1Interduction.py
@@ -0,0 +1,17 @@
+#1.2 Algorithms:
+
+#Algorithm Example 1 (English)
+#Ask for radius
+##Compute area by squaring radius and multiplying the result by pi
+##Display the computed area
+
+#Algorithm Example 2 (Pseudocode)
+#Ask for radius
+#let area = (radius2) × π
+#Display area
+
+
+radius = int(input("Enter the radius:"))
+area = (radius * radius) * 3.1415
+print("The area of a circle with radius", radius, "is:", area)
+
