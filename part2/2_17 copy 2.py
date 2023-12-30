b88b0315d921e218f885b3cd3a3ea8b7524eaa8b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2023-12-30 23:23:23 +0100

    2_17 copy 2.py aktualisieren

--- part2/2_17 copy 2.py
+++ part2/2_17 copy 2.py
@@ -8,7 +8,7 @@ and it will tell you the number of day of the week you will return on.
 '''
 # Define the days of the week , we do not need the whole name of them
 days_of_week = ["Su", "Mo", "Tu", "Wed", "Thu", "Fr", "Sa"]
-
+days_of_Month = ["Su", "Mo", "Tu", "Wed", "Thu", "Fr", "Sa"]
 # Ask the user for the starting day number (0 to 6)
 start_day_number = int(input("Enter the starting day number (0 for Sunday, 1 for Monday, ..., 6 for Saturday): "))
 
@@ -17,6 +17,7 @@ length_of_stay = int(input("Enter the length of your stay in nights: "))
 
 # Calculate the return day number using the days_of_week list
 return_day_number = (start_day_number + length_of_stay) % 7
+
 
 # Output the day of the week you will return on
 print(f"You will return home on a {days_of_week[return_day_number]}.")
