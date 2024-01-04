03e5ff1d73b521677d1f6dbe98034e6fd4d8621b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-04 16:09:44 +0000

    DeletMoreCopy

---
+++ part4/4PythonModules copy 3.py
@@ -0,0 +1,9 @@
+#the random Number 
+'''
+we must konw the random Number must be the exact form for Style:
+if we want the random Number from 23 untill 100 :
+we must the range btween 23 and 101
+
+'''
+
+
