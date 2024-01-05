bc51a75adabfd7cbe6e08375ee1da050978f01d6
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-05 16:57:24 +0000

    theSyntaxError

---
+++ part3/part3debugging copy 2.py
@@ -0,0 +1,7 @@
+# in this part we focus to the Debugging at the python script:
+
+'''
+at the first we have Pranteze Bug or Synatx:
+or t it names EOF synatx Error!
+'''
+print ("hello !"
---
+++ part3/part3debugging copy 3.py
@@ -0,0 +1,14 @@
+# in this part we focus to the Debugging at the python script:
+
+#you can see the error as 3.6. Runtime Errors:
+'''
+subtotal = input("Enter total before tax:")
+tax = .08 * subTotal
+print("tax on", subtotal, "is:", tax)
+'''
+
+#but the correct scripts is :
+subtotal = int(input("Enter total before tax:"))
+tax = 0.08 * subTotal
+print("tax on", subtotal, "is:", tax)
+
