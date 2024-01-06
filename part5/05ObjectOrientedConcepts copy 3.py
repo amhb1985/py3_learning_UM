4cb785440bab32653f9be57c31c3cd0ae1530593
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-06 12:03:45 +0000

    Turtule05testAvocado

--- part5/05ObjectOrientedConcepts copy 3.py
+++ part5/05ObjectOrientedConcepts copy 3.py
@@ -1,3 +1,15 @@
 #Object Oriented Concepts¶
 
+# draw a square
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("gold")
+
+#paint
+topf = turtle.Turtle()
+topf.pensize(30)
+topf.color("black")
+top.forward(30)
+
 
--- part5/05ObjectOrientedConcepts copy 4.py
+++ part5/05ObjectOrientedConcepts copy 4.py
@@ -1,3 +1,2 @@
 #Object Oriented Concepts¶
 
-
