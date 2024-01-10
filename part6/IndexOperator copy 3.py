4f2bbb9d6b4bfa2ce819040b475eca96fcb25de3
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-10 16:08:39 +0000

    part6Index03

--- part6/IndexOperator copy 3.py
+++ part6/IndexOperator copy 3.py
@@ -1,7 +1,7 @@
 #another test with the Numbers as Index:
 
 
-
+'''
 numbers = [17, 123, 87, 34, 66, 3448, 44]
 print(numbers[2])
 print(numbers[9-8])
@@ -10,5 +10,13 @@ print(numbers[-2])
 
 #when we treid withoud the name of varible :
 print([2-3])    #you can see this as one Index but after Math
+'''
 
+#in this way Py try to mix the exact character of script
+s = "python rocks"
+print(s[3] + s[-4])
+
+#you can see that inside of INDEX we can see all type of var:
+alist = [3, 67, "cat", [56, 57, "dog"], [ ], 3.14, False]
+print(alist[2])
 
