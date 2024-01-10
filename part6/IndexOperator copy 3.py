404b332db21a10d79abeceacf26a2cc221f664c1
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-10 15:48:09 +0000

    part6Index02

---
+++ part6/IndexOperator copy 3.py
@@ -0,0 +1,14 @@
+#another test with the Numbers as Index:
+
+
+
+numbers = [17, 123, 87, 34, 66, 3448, 44]
+print(numbers[2])
+print(numbers[9-8])
+print(numbers[-2])
+
+
+#when we treid withoud the name of varible :
+print([2-3])    #you can see this as one Index but after Math
+
+
