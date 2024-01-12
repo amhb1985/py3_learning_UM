2c882ab0684e79cfeaf7cc71af3f027e0f1a48c7
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-12 18:22:57 +0000

    subLst

--- part6/IndexOperator copy 4.py
+++ part6/IndexOperator copy 4.py
@@ -17,8 +17,13 @@ print(type(infos))
 
 #add another paragraf or exactly Index to the tuple
 julia = julia[:3] + ("eat ,Pray, love", 2010) + julia[:5]
-print(julia)
+#print(julia)
 
 # NOTE: in the list but it will not working!!
 #infos = [:1] + ["let do it for "] + [:3]
-print(infos)
+#print(infos)
+
+#next we try to make one SubGroup from one main
+new_lst = ["computer", "luxurious", "basket", "crime", 0, 2.49, "institution", "slice", "sun", ["water", "air", "fire", "earth"], "games", 2.7, "code", "java", ["birthday", "celebration", 1817, "party", "cake", 5], "rain", "thunderstorm", "top down"]
+sub_lst = new_lst[8:12]
+print (sub_lst)
