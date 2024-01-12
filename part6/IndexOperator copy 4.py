21d321cf30f809ff82761faf6c1c9b1dd0b9309a
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-12 18:00:41 +0000

    testLencommand

--- part6/IndexOperator copy 4.py
+++ part6/IndexOperator copy 4.py
@@ -1,9 +1,16 @@
 #so, today we are going again testing with The Slice Operator:
 
 julia = ("Julia","Roberr", 1967,"Duplicity",1988,1976, "Georg" )
+infos = [1, 22 , "Golf"]
 
+'''
 print(julia[2])
 print(julia[2:4])
 
 #asking abou type of list or tuple
 print(type(julia))
+print(type(infos))
+'''
+
+print(len(julia))
+print(len(infos))
