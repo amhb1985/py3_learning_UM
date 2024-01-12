e1fb21937141e30d1956c7d1e49456f681a5de06
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-12 17:58:18 +0000

    sliceOperator

---
+++ part6/IndexOperator copy 4.py
@@ -0,0 +1,9 @@
+#so, today we are going again testing with The Slice Operator:
+
+julia = ("Julia","Roberr", 1967,"Duplicity",1988,1976, "Georg" )
+
+print(julia[2])
+print(julia[2:4])
+
+#asking abou type of list or tuple
+print(type(julia))
