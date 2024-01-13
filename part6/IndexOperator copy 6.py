be89ec2c568bf8470c5ec72307e4b43b9bf92f7e
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-13 20:17:05 +0000

    SliceOperator

---
+++ part6/IndexOperator copy 6.py
@@ -0,0 +1,12 @@
+'''
+this is the py test for the index
+and about some training in Py3 
+'''
+#defintion one Variable :
+school = "Marthin Luther"
+
+#making from one Var file to one Index file:
+m = school[3]
+print(m)
+lastchar = school[-1]
+print(lastchar)
