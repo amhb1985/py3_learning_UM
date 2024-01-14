1387f6a5dd78c7cc5fc5e873b1927b55ffe92639
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-14 17:09:51 +0000

    TheLastinList

---
+++ part6/IndexOperator copy 9.py
@@ -0,0 +1,14 @@
+'''
+this is the py test for the index
+and about some training in Py3 
+'''
+
+# so hier ist the test at the end of  Lection:
+
+
+sports = ["football","tennis","bascetbll","volyball","Chess"]
+
+
+#we can set the list as the last 3 with -3:
+last = sports[-3:]
+print (last)
