a0c1af10118921d3fc6dc8a9218d024c445e772e
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-29 07:45:58 +0000

    testLOOP

---
+++ part7/Iteration copy 6.py
@@ -0,0 +1,10 @@
+#learning again around the Loop
+
+print ("this is repeatly LOOP!:")
+
+for _ in range (2):
+    print ("Hi,")
+    print("How are U?")
+
+
+print ("this is th e End of loop after 2 time")
---
+++ part7/Iteration copy 7.py
@@ -0,0 +1,9 @@
+
+numbers = [1, 3, 4, 2, 99 , 22, 23, 87]
+
+accum = 0
+
+for w in numbers:
+    accum = accum + w
+
+    print (accum)
