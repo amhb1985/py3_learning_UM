c9bb1d8ae52effff5f26bd2e1b5de9aefce42da0
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-30 10:38:52 +0000

    InstructionText

--- part7/iteration copy 10.py
+++ part7/iteration copy 10.py
@@ -6,10 +6,16 @@ import image
 
 #2. inpuut the imagae
 img = image.Image("luther.jpg")
+
+#3.set imgage window and frame , 
 win = image.ImageWin(img.getWidth(), img.getHeight())
+
+#4. command to draw and delay time
 img.draw(win)
 img.setDelay(1,15)   # setDelay(0) turns off animation
 
+
+#5. use one Loop to regular command to changing the colours
 for row in range(img.getHeight()):
     for col in range(img.getWidth()):
         p = img.getPixel(col, row)
@@ -22,5 +28,6 @@ for row in range(img.getHeight()):
 
         img.setPixel(col, row, newpixel)
 
+#6. set the final image draw
 img.draw(win)
 win.exitonclick()
