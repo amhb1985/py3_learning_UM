da854d3e3084d0364ba3670350be8aa6a0e8d69f
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-30 10:22:04 +0000

    infoRGB

--- part7/iteration copy 12.py
+++
@@ -1,6 +0,0 @@
-#we can iterate through the indexes by generating a sequence of them, using the range function.
-
-fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
-
-for n in range(len(fruits)):
-    print(n, fruits[n])
