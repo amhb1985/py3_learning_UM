4d2accfe45c26e3a50c0f5cb3b8f9fb839eb01ca
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-01-30 10:17:29 +0000

    ImageObject

---
+++ part7/iteration copy 13.py
@@ -0,0 +1,6 @@
+#we can iterate through the indexes by generating a sequence of them, using the range function.
+
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+
+for n in range(len(fruits)):
+    print(n, fruits[n])
