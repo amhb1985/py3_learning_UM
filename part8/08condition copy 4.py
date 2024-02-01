c1d488058b0ad81af0254fd3fa84d2967d48ed7b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-01 10:33:39 +0000

    Not_In

--- part8/08condition copy 4.py
+++ part8/08condition copy 4.py
@@ -4,9 +4,17 @@
 #2. not in
 
 #the easiest sample for this is:
-print('p' in 'apple')
+'''print('p' in 'apple')
 print('i' in 'apple')
 print('ap' in 'apple')
 print('pa' in 'apple')
 
+print('a' in 'a')
+print('apple' in 'apple')
+print('' in 'a')
+print('' in 'apple')
+'''
+
+#2 NOT IN
+print('x' not in 'apple')
 
