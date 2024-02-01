cd22c49bf66976bf12ed3bd4a723ed0c025283c2
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-01 10:30:45 +0000

    IN_NOTIN_boolan_log_func

---
+++ part8/08condition copy 4.py
@@ -0,0 +1,12 @@
+#this is the test for the another Logical func in Boolan:
+
+#1. in
+#2. not in
+
+#the easiest sample for this is:
+print('p' in 'apple')
+print('i' in 'apple')
+print('ap' in 'apple')
+print('pa' in 'apple')
+
+
