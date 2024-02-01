31a5bfe077f2384bf219d8ba4755b38994c57a5a
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-01 09:42:49 +0000

    Turtle_In_Function

---
+++ part8/08condition copy.py
@@ -0,0 +1,23 @@
+# the Test said us ehats can we do with condition in turtle commend:
+#Notice becaus of the lack of turtle import the code will not working
+ import turtle
+wn = turtle.Screen()
+
+amy = turtle.Turtle()
+amy.pencolor("Pink")
+amy.forward(50)
+if amy.pionencolor() == "Pink":
+    amy.right(60)
+    amy.forward(100)
+else:
+    amy.left(60)
+    amy.forward(100)
+
+kenji = turtle.Turtle()
+kenji.forward(60)
+if kenji.pencolor() == "Pink":
+    kenji.right(60)
+    kenji.forward(100)
+else:
+    kenji.left(60)
+    kenji.forward(100)
