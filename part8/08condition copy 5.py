63eaf0e438fdda9d45596feaf85684671072b76b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-02 08:29:09 +0000

    if_else

---
+++ part8/08condition copy 5.py
@@ -0,0 +1,28 @@
+#so in this chapter we r going to Conditional Execution: Binary Selection:
+
+'''x = 0
+
+if x % 2 == 0:
+    print(x, "is even")
+else:
+    print(x, "is odd")
+'''
+#the Test:
+#Create a variable, b, and assign it the value of 15. Then, write code to see if the value b is greater than that of a. If it is, a’s value should be multiplied by 2. If the value of b is less than or equal to a,
+# nothing should happen. Finally, create variable c and assign it the value of the sum of a and b.
+
+# Given variables
+a = 10
+b = 15
+
+# Check if b is greater than a
+if b > a:
+    # Multiply a by 2 if b is greater than a
+    a *= 2
+
+# Calculate the sum of a and b
+c = a + b
+
+print("Value of a:", a)
+print("Value of b:", b)
+print("Value of c:", c)
