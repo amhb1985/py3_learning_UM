51f7a19d601ef0e618b3ce6795aa443e00d0dfa2
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-02 17:16:51 +0000

    add_in_list

--- part9/TransformingSequences copy.py
+++ part9/TransformingSequences copy.py
@@ -6,3 +6,18 @@ print(fruit)
 fruit[0] = "pear"
 fruit[-1] = "Kiwi"
 print(fruit)
+
+
+#remove or delet from list:
+alist = ['a', 'b', 'c', 'd', 'e', 'f']
+alist[1:3] = []
+print(alist)
+
+
+#insert or add one data to list (with the number)
+alist = ['a', 'd', 'f']
+alist[1:1] = ['b', 'c']
+print(alist)
+
+alist[4:4] = ['e']
+print(alist)
