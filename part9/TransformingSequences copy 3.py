474e19d85222438828b7695dd445f21b1f559e59
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-03 09:21:27 +0000

    NonMutating_Methods_Strings

--- part9/TransformingSequences copy 3.py
+++ part9/TransformingSequences copy 3.py
@@ -1,1 +1,26 @@
-Transforming Sequences
+#Non-mutating Methods on Strings
+'''
+upper
+none
+Returns a string in all uppercase
+lower
+none
+Returns a string in all lowercase
+count
+item
+Returns the number of occurrences of item
+index
+item
+Returns the leftmost index where the substring item is found and causes a runtime error if item is not found
+strip
+none
+Returns a string with the leading and trailing whitespace removed
+replace
+old, new
+Replaces all occurrences of old substring with new
+format
+substitutions
+Involved! See String Format Method, below
+'''
+
+
