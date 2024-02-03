2638bee5063bbd18ea821626472bf4476294e506
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-03 09:25:58 +0000

    end_Non-mutating Methods on Strings

--- part9/TransformingSequences copy 3.py
+++ part9/TransformingSequences copy 3.py
@@ -1,31 +1,52 @@
 #Non-mutating Methods on Strings
 '''
-upper
+.upper()
 none
 Returns a string in all uppercase
-lower
+
+.lower()
 none
 Returns a string in all lowercase
-count
+
+.count()
 item
 Returns the number of occurrences of item
-index
+
+.index()
 item
 Returns the leftmost index where the substring item is found and causes a runtime error if item is not found
-strip
+
+.strip()
 none
 Returns a string with the leading and trailing whitespace removed
-replace
+
+.replace()
 old, new
 Replaces all occurrences of old substring with new
 format
 substitutions
-Involved! See String Format Method, below
+Involved! See String Format Method
 '''
-
+#upper and lower
 ss = "Hello, World"
 print(ss.upper())
 
 tt = ss.lower()
 print(tt)
 print(ss)
+
+
+#count & strip & replace
+ss = "    Hello, World    "
+els = ss.count("l")
+print(els)
+
+print("***"+ss.strip()+"***")
+
+news = ss.replace("o", "***")
+print(news)
+
+
+#counting math
+s = "python rocks"
+print(s.count("o") + s.count("p"))
