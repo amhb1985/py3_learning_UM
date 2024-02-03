191827a535cd093b8c3ecb1af97b8d1dfe06683b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-03 09:43:37 +0000

    changing_test

--- part9/TransformingSequences copy 4.py
+++ part9/TransformingSequences copy 4.py
@@ -1,8 +1,17 @@
 #testing againg with another example and tests:
 
 
-#test with name
+#test with name and scre and print it in last 
+#please Note to set str (string) command for score!
 name = "Amir Habibi"
 score = 1  # No respect!
 print("Hello " + name + ". Your score is " + str(score))
 
+
+
+#score and set info 
+scores = [("amir", -1), ("Marlon ", 1), ("eli", 100)]
+for person in scores:
+    name = person[0]
+    score = person[1]
+    print("Hello " + name + ". Your score is " + str(score))
