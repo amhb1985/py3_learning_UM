87a8c13274e21eb0aebc2c7f9f4d8562ff43fecf
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-03 10:01:16 +0000

    set_Price_Off

--- part9/TransformingSequences copy 6.py
+++ part9/TransformingSequences copy 6.py
@@ -1,4 +1,14 @@
 #some test with input :
-
+'''
 person = input('Enter your name: ')
 print('Hello {}!'.format(person))
+
+'''
+
+#set and calculate price and discount:
+
+origPrice = float(input('Enter the original price: $'))
+discount = float(input('Enter discount percentage: '))
+newPrice = (1 - discount/100)*origPrice
+calculation = '${} discounted by {}% is ${}.'.format(origPrice, discount, newPrice)
+print(calculation)
