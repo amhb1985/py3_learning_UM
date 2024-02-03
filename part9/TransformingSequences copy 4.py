f35e5f5697f22339926bcede60e10bcd3716341f
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-03 09:34:31 +0000

    testing_in_string

--- part9/TransformingSequences copy 4.py
+++ part9/TransformingSequences copy 4.py
@@ -1,1 +1,8 @@
-Transforming Sequences
+#testing againg with another example and tests:
+
+
+#test with name
+name = "Amir Habibi"
+score = 1  # No respect!
+print("Hello " + name + ". Your score is " + str(score))
+
