213a603ddba03fae04e51b4b3ff12cd8dd40a051
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-03 10:21:16 +0000

    explain_the_proces

--- part9/TransformingSequences copy 7.py
+++ part9/TransformingSequences copy 7.py
@@ -1,8 +1,20 @@
 #Accumulator Pattern with Lists¶
 
+
+#1. set an list as Numbers:
 nums = [3, 5, 8]
+
+#2. set the newlist for Accuulation
 accum = []
+
+#3. set Loop for each Element 
 for w in nums:
+
+    #3.01 in every element in list "nums" macke an math as name "x":
     x = w**2
+
+    #03.02 append or add this loop as "x" to the list:
     accum.append(x)
+
 print(accum)
+print(nums)
