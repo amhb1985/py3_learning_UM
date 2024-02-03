2c83e90dc041563a44d510a3e107601883681779
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-03 10:08:54 +0000

    explain_test

--- part9/TransformingSequences copy 6.py
+++ part9/TransformingSequences copy 6.py
@@ -7,8 +7,25 @@ print('Hello {}!'.format(person))
 
 #set and calculate price and discount:
 
+#1. set Price AND Discount >>> as Input and type as float:
 origPrice = float(input('Enter the original price: $'))
 discount = float(input('Enter discount percentage: '))
+
+#2. Calculate new Price after discount
 newPrice = (1 - discount/100)*origPrice
+
+#3. Set an Explaination to show the result:
 calculation = '${} discounted by {}% is ${}.'.format(origPrice, discount, newPrice)
+print(calculation)
+
+
+#1 inputs:
+origPrice = float(input('Enter the original price: $'))
+discount = float(input('Enter discount percentage: '))
+
+#2 formula & calculate:
+newPrice = (1 - discount/100)*origPrice
+
+#2. Eplaination
+calculation = '${:.2f} discounted by {}% is ${:.2f}.'.format(origPrice, discount, newPrice)
 print(calculation)
