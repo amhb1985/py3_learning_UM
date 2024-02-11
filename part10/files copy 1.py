6bcd8e64ea728171d4fb8e73328d828edc641e24
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-11 17:27:18 +0000

    _proble_with_opening_in_git

--- part10/files copy 1.py
+++ part10/files copy 1.py
@@ -3,11 +3,12 @@
 
 
 #Note: cause of dont existing the olympics.txt here we can not see the result under the terminal
-fileref = open("info.txt", "r")
+#fileref = open("info.txt", "r")
 
 #fileref : (/workspaces/py3_learning_UM/part10/mydata.txt)
 ## other code here that refers to variable fileref
-fileref.close()
+#fileref.close()
 
-
-
+file_test_read = open("workspaces/py3_learning_UM/part10/mydata.txt","r")
+print(file_test_read)
+file_test_read.close()
