01d71d6f4c80a57145c39e23043847e483fcc934
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-11 17:40:44 +0000

    testing_another_txt_data

---
+++ part10/files copy 6.py
@@ -0,0 +1,19 @@
+#testing_another_txt_data
+#part 1: 
+for number in range(1, 5):
+    square = number * 2
+    print(square)
+    
+#part 2.
+'''filename = "squared_numbers.txt"
+outfile = open(filename, "w")
+
+for number in range(1, 13):
+    square = number * number
+    outfile.write(str(square) + "\n")
+
+outfile.close()
+
+infile = open(filename, "r")
+print(infile.read()[:10])
+infile.close()'''
