f8b653414325dd3799ccec9609500e454ddbc8d5
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-11 17:46:40 +0000

    set_another_text_data

--- part10/files copy 6.py
+++ part10/files copy 6.py
@@ -1,19 +1,22 @@
 #testing_another_txt_data
-#part 1: 
+#part 1(it just show in Terminal): 
+'''
 for number in range(1, 5):
     square = number * 2
     print(square)
-    
-#part 2.
-'''filename = "squared_numbers.txt"
+    '''
+#part 2.(set and creat a file and then wirte in it)
+filename = "number_x_2.txt"
 outfile = open(filename, "w")
 
-for number in range(1, 13):
-    square = number * number
+for number in range(1, 5):
+    square = number * 2
     outfile.write(str(square) + "\n")
 
 outfile.close()
 
+'''
 infile = open(filename, "r")
 print(infile.read()[:10])
-infile.close()'''
+infile.close()
+'''
