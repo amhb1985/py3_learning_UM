09dd44da3162b75470a4a4f9fb85ff18e2fa17c8
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-11 17:16:04 +0000

    copy_an_text_file_path

--- part10/files copy 1.py
+++ part10/files copy 1.py
@@ -4,6 +4,8 @@
 
 #Note: cause of dont existing the olympics.txt here we can not see the result under the terminal
 fileref = open("info.txt", "r")
+
+#fileref : (/workspaces/py3_learning_UM/part10/mydata.txt)
 ## other code here that refers to variable fileref
 fileref.close()
 
