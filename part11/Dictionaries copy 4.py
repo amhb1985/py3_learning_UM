d571da26a1d7160a787eea4704da65efc48b7cea
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-12 15:35:14 +0000

    Got_key_in_Dictionary

--- part11/Dictionaries copy 4.py
+++ part11/Dictionaries copy 4.py
@@ -1,5 +1,5 @@
 #11.4.1. Iterating over Dictionaries
-
+'''
 inventory = {'apples': 430, 'bananas': 312, 'pears': 217, 'oranges': 525}
 
 for akey in inventory.keys(): 
@@ -10,3 +10,11 @@ ks = list(inventory.keys())       # Make a list of all of the keys
 print(ks)
 print(ks[0])                      # Display the first key
 
+
+'''
+inventory02 = {'apples': 430, 'bananas': 312, 'oranges': 525, 'pears': 217}
+
+for k in inventory02:
+    print("Got key", k)
+
+    
