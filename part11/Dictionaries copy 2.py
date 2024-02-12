04436b511842a0fcacb22c4d62cd40a28bafffc0
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-12 10:04:52 +0000

    another_example_with_medals

--- part11/Dictionaries copy 2.py
+++ part11/Dictionaries copy 2.py
@@ -7,3 +7,19 @@
 zoo_list = {"cat":12, "dog":6, "elephant":23}
 print(zoo_list["cat"])
 
+#for another example we must notice that we can
+#use another form with '':
+
+#create a dictionary
+olympics = {
+    'gold' : 7,
+    'silver' : 8,
+    'bronze' : 6
+}
+
+#print al of the medals in olympics
+print(olympics)
+
+# for one exact key we can use this form:
+print(olympics['gold'])
+
