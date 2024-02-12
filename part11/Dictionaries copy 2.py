35e5944fc0eae01a10dbdcb5bb286e8b0d95b0a0
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-12 09:36:21 +0000

    Zoo_list_dictionary

--- part11/Dictionaries copy 2.py
+++ part11/Dictionaries copy 2.py
@@ -1,1 +1,9 @@
-#
+# the some sample is :
+#we can set one key with one exact varible in the List:
+#for example here  we can set some information for the agae of every animail of a zoo 
+#with info as Integers valus:
+
+
+zoo_list = {"cat":12, "dog":6, "elephant":23}
+print(zoo_list["cat"])
+
