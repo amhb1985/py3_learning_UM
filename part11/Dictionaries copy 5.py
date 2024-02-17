62a048cd7f891dbe58402aec63833ff27f754f6e
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-17 11:55:11 +0000

    create_a_Dic

---
+++ part11/Dictionaries copy 5.py
@@ -0,0 +1,13 @@
+
+# 11.12. Chapter Assessment¶
+'''
+test 1:
+
+At the halfway point during the Rio Olympics, the United States had 70 medals, Great Britain had 38 medals, China had 45 medals,
+Russia had 30 medals, and 
+Germany had 17 medals. Create a dictionary assigned to the variable medal_count w
+ith the country names as the keys and the number of medals the country had as each key’s value.
+
+'''
+
+
---
+++ part11/Dictionaries copy 6.py
@@ -0,0 +1,17 @@
+my_dict = {}  # Create an empty dictionary
+
+# Add a key-value pair
+# NOTICE : we must have just ONE KEY > ONE VALUE
+#my_dict["key"] = "value"
+
+# Example
+
+my_dict["John"] = 30
+
+my_dict["joe"] = 20
+
+#print all of the Dic
+print(my_dict)
+
+#print the one of exact Key:
+print(my_dict["joe"])
