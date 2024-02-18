facd971ca09aecdcb697ba5cfacb8af12e8f6fea
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-18 15:13:26 +0000

    2nd_def_func_list

---
+++ part12/Function copy 3.py
@@ -0,0 +1,12 @@
+
+
+
+#with def func we can hide our code/scripte under^
+
+#(s) is here a list to put some var in it_ like str: "Iman" and "Jackie" or "Amir"
+def hello2(s):
+    print("Hello " + s)
+    print("Glad to meet you")
+
+hello2("Iman" + " and Jackie")
+hello2("Class " * 3)
