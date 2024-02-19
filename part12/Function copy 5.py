50be986495a40dcaed03fb40d5e17ddd6eae70ce
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-19 16:05:28 +0000

    Test_Func_12_05_2

--- part12/Function copy 5.py
+++ part12/Function copy 5.py
@@ -1,11 +1,15 @@
 # we try again with the another Func 
 
 
+
+#this is a func to set an Formula like square
 def square(x):
     y = x * x
     return y
 
 toSquare = 10
 result = square(toSquare)
+
+
 print("The result of {} squared is {}.".format(toSquare, result))
  
