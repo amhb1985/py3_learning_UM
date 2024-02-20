bbd1d3e6b55d3b729fa769d2e80368f8fca41fb5
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-20 16:53:56 +0000

    aother_func

--- part12/Function copy 7.py
+++ part12/Function copy 7.py
@@ -12,10 +12,41 @@ print(square(square(2)))
 
 #Annotations
 
-def duplicate(msg: str) -> str:
+# we create one Function with the some index commend for example here will : " duplicate "
+# and then we wil try set an exact type to our Function inside of ()
+
+'''def duplicate(msg: str) -> str:
     """Returns a string containing two copies of `msg`"""
 
     return msg + msg
 
-result = duplicate('Hello ... ')
+result = duplicate('Hello ... again ->  ')
 print(result)
+'''
+'''
+def add(x: int, y: int) -> int:
+    """Returns the sum of `x` and `y`"""
+
+    return x + y
+
+def get_number(msg: str) -> float:
+    """Prompts with `msg` for input; returns numeric response."""
+
+    return float(input(msg))
+
+def display_msg(msg: str):
+    """Displays `msg` with dashed line underneath"""
+
+    print(msg)
+    print('-------------------------------------')
+
+
+'''
+
+#test to input
+#display = input("write your Number:")
+def display(msg: str):
+    """Displays `msg` on the screen"""
+    print(msg + 2)
+
+display()
