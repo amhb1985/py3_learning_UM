3a33da6df83199bdadfdfa579379b2ec6871b625
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-20 17:01:08 +0000

    accumulates

--- part12/Function copy 9.py
+++ part12/Function copy 9.py
@@ -3,6 +3,19 @@
 
 #its easy we can just set first an function with def and the name with Number
 # and then we must use the -3 for return
-
+'''
 def subtract_three(number):
     return number - 3
+'''
+## A accumulates / Funcion 
+#so 1. we creare a func and then..
+#2. we name seq
+def mylen(seq):
+    c = 0 # initialize count variable to 0
+    for _ in seq:
+        c = c + 1   # increment the counter for each item in seq
+    return c
+
+#so we can know calculate all of the words or Numbers withe set in mylen()!
+print(mylen("hello"))
+print(mylen([1, 2, 7]))
