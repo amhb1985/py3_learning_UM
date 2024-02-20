fbdcf13999561efa735507962171550839fc9b6f
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-20 13:52:19 +0000

    so_nechste_Anderunge

--- part12/Function copy 8.py
+++ part12/Function copy 8.py
@@ -2,7 +2,7 @@
 
 #What will the following code output?
 
-def cyu2(s1, s2):
+'''def cyu2(s1, s2):
     x = len(s1)
     y = len(s2)
     return x-y
@@ -12,3 +12,16 @@ if z > 0:
     print("First one was longer")
 else:
     print("Second one was at least as long")
+    '''
+
+
+def show_me_numbers(list_of_ints):
+    print(10)
+    print("Next we'll accumulate the sum")
+    accum = 0
+    for num in list_of_ints:
+        accum = accum + num
+    return accum
+    print("All done with accumulation!")
+
+show_me_numbers([4,2,3])
---
+++ part12/Function copy 9.py
@@ -0,0 +1,8 @@
+#Write a function called subtract_three that takes an integer 
+#or any number as input, and returns that number minus three.
+
+#its easy we can just set first an function with def and the name with Number
+# and then we must use the -3 for return
+
+def subtract_three(number):
+    return number - 3
