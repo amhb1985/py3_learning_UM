40573f97fdbd9f9fbac9c2b446d5bfad4523d15e
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-20 16:32:42 +0000

    Annotations_12_07

--- part12/Function copy 7.py
+++ part12/Function copy 7.py
@@ -2,9 +2,20 @@
 
 #test
 
-
+'''
 def square(x):
     y = x * x
     return y
 
 print(square(square(2)))
+'''
+
+#Annotations
+
+def duplicate(msg: str) -> str:
+    """Returns a string containing two copies of `msg`"""
+
+    return msg + msg
+
+result = duplicate('Hello ... ')
+print(result)
