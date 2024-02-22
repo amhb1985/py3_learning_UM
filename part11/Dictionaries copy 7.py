f28732c65924029b09faf8f3f9578cd91d0ef315
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-22 07:11:12 +0000

    2nd_Q_Dic

--- part11/Dictionaries copy 7.py
+++ part11/Dictionaries copy 7.py
@@ -17,3 +17,18 @@ medal_count = {
     "Germany": 17
 }
 print(medal_count.values())
+
+
+'''#2. Given the dictionary swimmers, 
+add an additional key-value pair to the dictionary 
+with "Phelps" as the key and the integer 23 as the value. Do not rewrite the entire dictionary.
+'''
+
+
+swimmers = {'Manuel':4, 'Lochte':12, 'Adrian':7, 'Ledecky':5, 'Dirado':4}
+
+swimmers["Phelps"] = 23
+
+print(swimmers)
+#print (swimemrs["Phelps"])
+
