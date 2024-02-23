c96a9d74752920e1949aa8e014d6fbc3c27e7045
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-23 22:26:04 +0000

    changeit_commend_in_function

--- part12/Function.py
+++ part12/Function.py
@@ -4,7 +4,7 @@
 
 def changeit(lst):
     lst[0] = "Berlin"
-    lst[1] = "Rom"
+    lst[1] = "atlanta"
 
 
 mylst = ['our', 'students', 'are', 'awesome']
@@ -12,3 +12,4 @@ mylst = ['our', 'students', 'are', 'awesome']
 #we can change one list as func with command : changeit(). 
 changeit(mylst)
 print(mylst)
+
