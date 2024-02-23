3a0e4fc2e7f753bdc9fc67b6eabb10e93f63a65b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-23 22:33:14 +0000

    del_lst_in_function

--- part12/Function.py
+++ part12/Function.py
@@ -12,7 +12,7 @@ mylst = ['our', 'students', 'are', 'awesome']
 #we can change one list as func with command : changeit(). 
 changeit(mylst)
 print(mylst)
-'''
+
 
 def myfun(lst):
     lst = [1, 2, 3]
@@ -21,4 +21,17 @@ mylist = ['a', 'b']
 
 #chnage all of list
 myfun(mylist)
+print(mylist)
+'''
+
+#change the list with commend del list  :
+def myfun(lst):
+    #we can set am exact delet to function:
+    del lst[1]
+
+mylist = ['a', 'b', 'c','d']
+
+myfun(mylist)
+
+#here we can see the change just the exact Index 
 print(mylist)
