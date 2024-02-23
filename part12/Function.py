3fccdeb63a1b169089f2597d879518cbb4fcaacb
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-23 22:25:17 +0000

    answere

---
+++ part12/Function.py
@@ -0,0 +1,14 @@
+
+
+#chnaging the list of student to City name
+
+def changeit(lst):
+    lst[0] = "Berlin"
+    lst[1] = "Rom"
+
+
+mylst = ['our', 'students', 'are', 'awesome']
+
+#we can change one list as func with command : changeit(). 
+changeit(mylst)
+print(mylst)
