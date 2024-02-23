3024e33e60e3dbe4a8e30235d15baf4c8e289048
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-23 22:28:43 +0000

    change_all_list

--- part12/Function.py
+++ part12/Function.py
@@ -1,7 +1,7 @@
 
 
 #chnaging the list of student to City name
-
+'''
 def changeit(lst):
     lst[0] = "Berlin"
     lst[1] = "atlanta"
@@ -12,4 +12,13 @@ mylst = ['our', 'students', 'are', 'awesome']
 #we can change one list as func with command : changeit(). 
 changeit(mylst)
 print(mylst)
+'''
 
+def myfun(lst):
+    lst = [1, 2, 3]
+
+mylist = ['a', 'b']
+
+#chnage all of list
+myfun(mylist)
+print(mylist)
