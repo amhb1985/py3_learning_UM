f80585f017b49ca528410109810fc6a1e3a32ab0
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-23 16:53:49 +0000

    another_func

--- part12/Function copy 10.py
+++ part12/Function copy 10.py
@@ -13,21 +13,12 @@ print(z)
 '''
 
 #first we can creat the dictionary 
-numbers = [1, 12, 13, 4]
-
 #then we create the def as name bar and back again with return 
-def foo(bar):
-    aug = str(bar) + "street"
-    return aug
+def badsquare(x):
+    y = x ** power
+    return y
 
-addresses = []
-for item in numbers:
-    addresses.append(foo(item))
-
-
-
-
-
-
-
+power = 2
+result = badsquare(10)
+print(result)
 
