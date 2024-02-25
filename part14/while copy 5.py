b791f50859b91a1aee2ca28438bc96729360da5e
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-25 09:41:11 +0000

    break_and_continue_in_while_LOOP

---
+++ part14/while copy 5.py
@@ -0,0 +1,9 @@
+#break and continue in while loop:
+
+while True:
+    print("this phrase will always print")
+    break
+    #continue # if we write continue this loop always contoniues...
+    print("Does this phrase print?")
+
+print("We are done with the while loop.")
