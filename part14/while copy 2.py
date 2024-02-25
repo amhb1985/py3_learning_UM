1d4d686e7bfa2b9a2f13c7bc2808412bff51374c
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-25 09:05:13 +0000

    explain_the_while

--- part14/while copy 2.py
+++ part14/while copy 2.py
@@ -6,10 +6,12 @@
 def sumTo(aBound):
     """ Return the sum of 1+2+3 ... n """
 
-    theSum  = 0
-    aNumber = 1
-    while aNumber <= aBound:
-        theSum = theSum + aNumber
+    theSum  = 0 #the sum so far
+    aNumber = 1 #where we are 1> 2> 3> 4 ...aBand
+
+    #whle ..... >>> return ..
+    while aNumber <= aBound:  #is less than or equal
+        theSum = theSum + aNumber #0 +1 = 1
         aNumber = aNumber + 1
     return theSum
 
