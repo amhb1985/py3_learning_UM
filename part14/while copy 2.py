0fd61f6d3b326f4687cbfc28a779e8c02daef961
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-25 09:00:16 +0000

    while_14_02

---
+++ part14/while copy 2.py
@@ -0,0 +1,18 @@
+
+
+#the while (Loop )statment:
+
+
+def sumTo(aBound):
+    """ Return the sum of 1+2+3 ... n """
+
+    theSum  = 0
+    aNumber = 1
+    while aNumber <= aBound:
+        theSum = theSum + aNumber
+        aNumber = aNumber + 1
+    return theSum
+
+print(sumTo(4))
+
+print(sumTo(1000))
