278a85156c99ab0604289d45ed922cc6b08eec67
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-29 19:08:49 +0100

    Update files copy 4.py

--- part10/files copy 4.py
+++ part10/files copy 4.py
@@ -1,6 +1,7 @@
 #another test for using "with" in one file:
 
 with open('mydata.txt', 'r') as md:
+#we must notice that we need this text data (mydata.txt)
 
     C. open("CompanyData/YearlyProjections.csv", "r")
     for line in md:
