2aec18733f9807b9bde43fd753f14a4143cc13a8
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-03-16 20:22:18 +0000

    nested_03

--- part17/nested_data.py
+++ part17/nested_data.py
@@ -10,5 +10,5 @@ print([10, 20, 30][1])
 # in another hand we can change th Index Number 
 print([20, 20, 40][2])
 
-
-#print(nested1[1][0])
+# for the first example as nested 1 we can do the same :
+print(nested1[1][0])
