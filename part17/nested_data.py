8d709aa7dead5e67d6d9dda10116886b7cfa6187
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-03-16 20:07:02 +0000

    nested_index

---
+++ part17/nested_data.py
@@ -0,0 +1,14 @@
+
+nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+y = nested1[1]
+print(y)
+print(y[0])
+
+# now we can see nested with this example : print ([ firs its a list with , , ,][the nuber of Index in list] )
+print([10, 20, 30][1])
+
+# in another hand we can change th Index Number 
+print([20, 20, 40][2])
+
+
+#print(nested1[1][0])
