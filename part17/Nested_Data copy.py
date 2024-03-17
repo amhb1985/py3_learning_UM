41da820d04771a5c9072e5cd90fb48141d128404
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-03-17 19:08:52 +0000

    changing_nested_test

--- part17/Nested_Data copy.py
+++ part17/Nested_Data copy.py
@@ -2,4 +2,16 @@
 # now we can  test the another question:
 nested2 = [{'a': 1, 'b': 3}, {'a': 5, 'c': 90, 5: 50}, {'b': 3, 'c': "yes"}]
 
-print(nested2([1][1]))
+# Print the value associated with key 'c' in the second dictionary (90)
+print(nested2[1]['c'])
+
+# Print the value associated with key 'b' in the third dictionary
+print(nested2[2]['b'])
+
+# Add a fourth dictionary at the end of the list
+nested2.append({'x': 10, 'y': 20})
+print(nested2)
+
+# Change the value associated with 'c' in the third dictionary from "yes" to "no"
+nested2[2]['c'] = "jo"
+print(nested2)
