0ce8ec95df47baffe10a8ca2239c41f4ee03a05b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-04 12:43:51 +0000

    training_with_type()

--- part8/08condition copy 2.py
+++ part8/08condition copy 2.py
@@ -7,5 +7,10 @@ print(type(True))
 print(type(False))
 '''
 #Look at this example to define :
-print(type(True))
-print(type("True"))
+#print(type(True))
+#print(type("True"))
+
+#another  example to do it again with Int and Float:
+print(type(13))
+print(type(13.2))
+# after that we can see the type of every element with type()
