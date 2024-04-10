dbbf8df9c68c00bcdc488f9d0426f2bc2971c735
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-10 05:28:47 +0000

    17.04_nested_iteration

--- part17/17_04_Nested_Iteration.py
+++ part17/17_04_Nested_Iteration.py
@@ -2,8 +2,34 @@
 #1.we need one list
 nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
 
-#2.we set an loop for x in 
+#2.we set an loop for x in our list (here is nested1) 
 for x in nested1:
     print("level1: ")
+    #3. again we set another loop inside of x
     for y in x:
         print("     level2: " + y)
+
+
+#another example:
+#Below, we have provided a list of lists that contain information about people. 
+#Write code to create a new list that contains every person’s last name, and save that list as last_names.
+
+info = [['Tina', 'Turner', 1939, 'singer'], ['Matt', 'Damon', 1970, 'actor'], ['Kristen', 'Wiig', 1973, 'comedian'], ['Michael', 'Phelps', 1985, 'swimmer'], ['Barack', 'Obama', 1961, 'president']]
+'''
+the whole of the cod is a simple like this:
+accum = []
+for somthing in somthing:#
+    <refer to accum>
+   '''
+#1.emptylist as lastname
+last_names = []
+
+#2. set a loop
+for entertainer in info:
+    #3.01 : set/define witch element (here last name is entertainer[1] )
+    #3.02 : then we must .append() it in to list
+    last_names.append(entertainer[1])
+    print(last_names)
+    
+
+
