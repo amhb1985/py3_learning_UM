62fbde20077c3cf000aa22c65b513b1fde1e0ba6
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-10 04:27:02 +0000

    17_03_JSON

---
+++ part17/17_3_nested_data_JSON.py
@@ -0,0 +1,14 @@
+ 
+#JSON : Java Script Object Notaion
+#its easy to wtite and edit 
+import json 
+
+
+a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
+print(a_string)
+d = json.loads(a_string)
+print("------")
+print(type(d))
+print(d.keys())
+print(d['resultCount'])
+# print(a_string['resultCount'])
