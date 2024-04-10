9259febe1f523576702ed579fcd46b1db855c74c
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-10 04:49:16 +0000

    loads_meaning

--- part17/17_3_nested_data_JSON copy 2.py
+++ part17/17_3_nested_data_JSON copy 2.py
@@ -5,7 +5,8 @@ import json
 
 a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
 print(a_string)
-d = json.loads(a_string)
+d = json.loads(a_string)    #here we can see the '.loads()' as mean load the Object in to String
+
 print("------")
 print(type(d))
 print(d.keys())
