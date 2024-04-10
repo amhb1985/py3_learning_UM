1b9305ae2da97075308aecafab247d27240c1144
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-10 05:08:28 +0000

    17_04_nested_iteration

--- part17/17_04_Nested_Iteration.py
+++ part17/17_04_Nested_Iteration.py
@@ -1,5 +1,8 @@
  
+#1.we need one list
 nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+
+#2.we set an loop for x in 
 for x in nested1:
     print("level1: ")
     for y in x:
--- part17/17_3_nested_data_JSON copy 2.py
+++ part17/17_3_nested_data_JSON copy 2.py
@@ -1,14 +1,28 @@
  
 #JSON : Java Script Object Notaion
 #its easy to wtite and edit 
+'''
 import json 
 
 a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
 print(a_string)
 d = json.loads(a_string)    #here we can see the '.loads()' as mean load the Object in to String
-
 print("------")
 print(type(d))
 print(d.keys())
-print(d['resultCount'])
-# print(a_string['resultCount'])
+print(d['resultCount']) # print(a_string['resultCount'])
+'''
+import json
+#so now we can set a nested file in our List:
+#1. we must define an object:
+def pretty(obj):
+    #2. we must set dumps():  Dump an Object in to String
+    return json.dumps(obj, sort_keys=True, indent=2)
+
+d = {'key1': {'c': True, 'a': 90, '5': 50}, 'key2':{'b': 3, 'c': "yes"}}
+
+print(d)
+print('--------')
+print(pretty(d))
+
+
