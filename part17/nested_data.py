6926648d6e6ecd5efd620d9d21ab657212bc6cd3
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-11 03:38:29 +0000

    little_test

--- part17/nested_data.py
+++ part17/nested_data.py
@@ -3,7 +3,7 @@ nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
 y = nested1[1]
 print(y)
 print(y[0])
-
+'''
 # now we can see nested with this example : print ([ firs its a list with , , ,][the nuber of Index in list] )
 print([10, 20, 30][1])
 
@@ -12,4 +12,6 @@ print([20, 20, 40][2])
 
 # for the first example as nested 1 we can do the same :
 print(nested1[1][0])
-
+'''
+# for the first example as nested 1 we can do the same :
+print(nested1[2][0])
