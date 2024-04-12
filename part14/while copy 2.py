756206ac9477085736dc0b77b052a205d3994aa4
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-12 11:01:13 +0000

    test_again_with_2

--- part14/while copy 2.py
+++ part14/while copy 2.py
@@ -4,9 +4,9 @@
 #definition a Sum 
 def sumTo(aBound):
     """ Return the sum of 1+2+3 ... n """
-
     theSum  = 0 #the sum so far
-    aNumber = 1 #where we are 1> 2> 3> 4 ...aBand
+    #we test again for anither test with 2
+    aNumber = 2 #where we are 1> 2> 3> 4 ...aBand
 
     #whle ..... >>> return ..
     while aNumber <= aBound:  #is less than or equal
