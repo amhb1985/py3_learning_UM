7cd82613954d028e20ece87c1027112f78afb14c
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-13 08:08:26 +0000

    nested_learning

--- part17/17_04_Nested_Iteration.py
+++ part17/17_04_Nested_Iteration.py
@@ -1,4 +1,5 @@
  
+'''
 #1.we need one list
 nested1 = [['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
 
@@ -9,18 +10,19 @@ for x in nested1:
     for y in x:
         print("     level2: " + y)
 
-
+'''
 #another example:
 #Below, we have provided a list of lists that contain information about people. 
 #Write code to create a new list that contains every person’s last name, and save that list as last_names.
 
+
 info = [['Tina', 'Turner', 1939, 'singer'], ['Matt', 'Damon', 1970, 'actor'], ['Kristen', 'Wiig', 1973, 'comedian'], ['Michael', 'Phelps', 1985, 'swimmer'], ['Barack', 'Obama', 1961, 'president']]
 '''
-the whole of the cod is a simple like this:
+#the whole of the cod is a simple like this:
 accum = []
-for somthing in somthing:#
+for somthing in somthing:
     <refer to accum>
-   '''
+ '''
 #1.emptylist as lastname
 last_names = []
 
@@ -29,7 +31,17 @@ for entertainer in info:
     #3.01 : set and define witch element (here last name is entertainer[1] )
     #3.02 : then we must .append() it in to list
     last_names.append(entertainer[1])
-    print(last_names)
+    #print(last_names)
+print(info[1][1])
+#for entertainer in info:
+    #for val in entertainer: 
+     #  print(val)
+
+
+
+
+
+
 
 
 
