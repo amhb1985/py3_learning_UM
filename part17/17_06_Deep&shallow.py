43812d71ebd69d1e7cec52634aa66732ca9dc4bf
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-14 15:45:36 +0000

    write_the_comment

--- part17/17_06_Deep&shallow.py
+++ part17/17_06_Deep&shallow.py
@@ -1,4 +1,5 @@
 
+#first we must have a List as Dictionary:
 original = [['dogs', 'puppies'], ['cats', "kittens"]]
 #we creat one list as copy with [:]
 copied_version = original[:]
@@ -7,5 +8,8 @@ print(copied_version is original)
 print(copied_version == original)
 original[0].append(["canines"])
 print(original)
+
+print("--------Now look at the copied version -----------")
 print("-------- Now look at the copied version -----------")
+
 print(copied_version)
