ef283ef303b90196d0c4d827c54428d3899f8fbf
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-14 15:49:03 +0000

    chnaging_the_list

--- part17/17_05_structing_nested_data.py
+++ part17/17_05_structing_nested_data.py
@@ -1,10 +1,10 @@
 
 
-nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h']]
+nested1 = [1, 2, ['a', 'b', 'c'],['d', 'e'],['f', 'g', 'h'],['A','B','C'],['a']]
 for x in nested1:
     print("level1: ")
     if type(x) is list:
         for y in x:
-            print("     level2: {}".format(y))
+            print("             level2: {}".format(y))
     else:
         print(x)
