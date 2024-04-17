a5b06ee78da042fcfcd1a6897f9f36a58959ae71
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-17 13:22:33 +0000

    another_ease_the_code

--- part17/17_07_Extracting_from_Nested_data.py
+++ part17/17_07_Extracting_from_Nested_data.py
@@ -772,6 +772,7 @@ res2 = res['statuses']
 # print("----Level 2: a list of tweets-----")
 # print(type(res2)) # it's a list!
 # print(len(res2))  # looks like one item representing each of the three tweets
+'''
 for res3 in res2[:1]:
    print("----Level 3: a tweet----")
    # print(json.dumps(res3, indent=2)[:30])
@@ -780,3 +781,11 @@ for res3 in res2[:1]:
    # print(type(res4)) # it's a dictionary
    # print(res4.keys()) after running we'll see the time of creat of it 
    print(res4['screen_name'], res4['created_at'])
+
+'''
+
+
+#we can easy write this code to do all of above(in another hand is samwe as above:):
+for res3 in res['statuses']:
+    print(res3['user']['screen_name'], res3['user']['created_at'])
+
