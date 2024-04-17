9726c828d4e653f116d3f12b113b8d8bba951b90
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-17 13:12:11 +0000

    another_extracting_from_nested_Level_4

--- part17/17_07_Extracting_from_Nested_data.py
+++ part17/17_07_Extracting_from_Nested_data.py
@@ -732,7 +732,7 @@ print(type(res2)) # it's a list!
 #and how many we have it
 print(len(res2))
 '''
-
+'''
 #another test for extracting something in tweet for the Level 3:
 import json
 print(type(res))
@@ -746,3 +746,16 @@ for res3 in res2[:1]:
    print(json.dumps(res3, indent=2)[:30])
    print(type(res3)) # it's a dictionary
    print(res3.keys())
+'''
+
+import json
+print(type(res))
+print(res.keys())
+res2 = res['statuses']
+print("----Level 2: a list of tweets-----")
+print(type(res2)) # it's a list!
+print(len(res2))  # looks like one item representing each of the three tweets
+for res3 in res2[:1]:
+   print("----Level 3: a tweet----")
+   print(json.dumps(res3, indent=2)[:30])
+   res4 = res3['user']
