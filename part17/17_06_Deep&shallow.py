765bd44444192753b094fc1b5e3b1ded34958445
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-17 12:31:23 +0000

    explain_the_3rd_part_of_shallow_copy

--- part17/17_06_Deep&shallow.py
+++ part17/17_06_Deep&shallow.py
@@ -30,4 +30,16 @@ print(original)
 print("-------- Now look at the copied version -----------")
 print(copied_outer_list)
 '''
-
+#import majol of copy
+import copy
+original = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
+shallow_copy_version = original[:]
+deeply_copied_version = copy.deepcopy(original)
+original.append("Hi there")
+original[0].append(["marsupials"])
+print("-------- our Original List :-----------")
+print(original)
+print("-------- deep copy  -----------")
+print(deeply_copied_version)
+print("-------- shallow copy -----------")
+print(shallow_copy_version)
