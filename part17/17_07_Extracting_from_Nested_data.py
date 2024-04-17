01de1bdf30d72ee6dcb7deb3b4493158153da56e
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-17 13:17:22 +0000

    create_info_like_time_in_extract_nested_data

--- part17/17_07_Extracting_from_Nested_data.py
+++ part17/17_07_Extracting_from_Nested_data.py
@@ -747,7 +747,7 @@ for res3 in res2[:1]:
    print(type(res3)) # it's a dictionary
    print(res3.keys())
 '''
-
+'''
 import json
 print(type(res))
 print(res.keys())
@@ -759,3 +759,24 @@ for res3 in res2[:1]:
    print("----Level 3: a tweet----")
    print(json.dumps(res3, indent=2)[:30])
    res4 = res3['user']
+
+   '''
+
+
+#now we can set our results with the time information , here for our result in Level 4:
+
+import json
+# print(type(res))
+# print(res.keys())
+res2 = res['statuses']
+# print("----Level 2: a list of tweets-----")
+# print(type(res2)) # it's a list!
+# print(len(res2))  # looks like one item representing each of the three tweets
+for res3 in res2[:1]:
+   print("----Level 3: a tweet----")
+   # print(json.dumps(res3, indent=2)[:30])
+   res4 = res3['user']
+   print("----Level 4: the user who wrote the tweet----")
+   # print(type(res4)) # it's a dictionary
+   # print(res4.keys()) after running we'll see the time of creat of it 
+   print(res4['screen_name'], res4['created_at'])
