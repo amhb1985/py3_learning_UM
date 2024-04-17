0f6578a7db9d3e0bc8a17e7d819c835576735099
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-17 12:28:53 +0000

    another_test_in_Deep_shallow_copy

--- part17/17_06_Deep&shallow.py
+++ part17/17_06_Deep&shallow.py
@@ -1,4 +1,4 @@
-
+'''
 #first we must have a List as Dictionary:
 original = [['dogs', 'puppies'], ['cats', "kittens"]]
 #we creat one list as copy with [:]
@@ -6,6 +6,7 @@ copied_version = original[:]
 print(copied_version)
 print(copied_version is original)
 print(copied_version == original)
+
 original[0].append(["canines"])
 print(original)
 
@@ -13,3 +14,20 @@ print("--------Now look at the copied version -----------")
 print("-------- Now look at the copied version -----------")
 
 print(copied_version)
+
+'''
+'''
+#2: 
+# our orginal list is:
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_outer_list = []
+for inner_list in original:
+    copied_inner_list = inner_list[:]
+    copied_outer_list.append(copied_inner_list)
+print(copied_outer_list)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_outer_list)
+'''
+
