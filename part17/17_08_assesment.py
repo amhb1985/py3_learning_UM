fd5461efe9fefd316c0a4fa4a818318935668c51
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-18 11:34:40 +0000

    Q_2_in_nested

--- part17/17_08_assesment.py
+++ part17/17_08_assesment.py
@@ -1,7 +1,31 @@
 
-
-#1. The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
+'''
+#Q1. The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
 nested = [['dog', 'cat', 'horse'], ['frog', 'turtle', 'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]
 #set an output nested[1][2]
 output = nested[1][2]
 print(output)
+'''
+#Q2:below, a list of lists is provided. Use in and not in tests to create variables
+# with Boolean values. See comments for further instructions.
+
+'''
+lst = [['apple', 'orange', 'banana'], [5, 6, 7, 8, 9.9, 10], ['green', 'yellow', 'purple', 'red']]
+
+#Test to see if 'yellow' is in the third list of lst. Save to variable ``yellow``
+#set a yellow stringfor searching in list [BUT WICH ONE??]
+yellow = 'yellow' in lst[2]
+
+#Test to see if 4 is in the second list of lst. Save to variable ``four``
+#set a 4 Int searching in list [BUT WICH ONE??]
+four = 4 in lst[1]
+print(four)
+
+
+#Test to see if 'orange' is in the first element of lst. Save to variable ``orange``
+#set a orange as string for searching in list [BUT WICH ONE??]
+orange = 'orange' in lst [0]
+print(orange)
+'''
+
+
