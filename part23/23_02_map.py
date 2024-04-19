e9bf788118d1de115ad04cd9a75e43ad8d743096
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-19 11:05:38 +0000

    set_another_int_and_change_Formula

--- part23/23_02_map.py
+++ part23/23_02_map.py
@@ -2,7 +2,7 @@
 #this is about Map in py 3
 
 
-#1. we set an definition for our Doublestuff
+#1. we set an definition Function for our Doublestuff
 def doubleStuff(a_list):
     """ Return a new list in which contains doubles of the elements in a_list. """
     #2.then we set an empty List:
@@ -10,7 +10,7 @@ def doubleStuff(a_list):
     #3. we need now one Loop for value in List
     for value in a_list:
         #3.1 set a Formula for a value
-        new_elem = 2 * value
+        new_elem = (2 * value)+1
         #3.2 Append (add) the results in our value
         new_list.append(new_elem)
         #4. at end,,,, return it
@@ -18,7 +18,7 @@ def doubleStuff(a_list):
 
 
 #so we can test it for one excample with Intege 
-things = [2, 5, 9]
+things = [4, 3, 9]
 print(things)
 things = doubleStuff(things)
 print(things)
