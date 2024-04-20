20863abfaee23e508b54f4daefb2e3a117584f50
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-20 10:23:25 +0000

    23_02_explain_filter

--- part23/23_03_filter.py
+++ part23/23_03_filter.py
@@ -1,7 +1,13 @@
 #this is about filter
+
+#1. creat one def
 def keep_evens(nums):
+    #2. creat a new empty List
     new_list = []
+
+    #3. set one Loop with for Number in Numbers:
     for num in nums:
+        #3_1 : set  filter with IF 
         if num % 2 == 0:
             new_list.append(num)
     return new_list
