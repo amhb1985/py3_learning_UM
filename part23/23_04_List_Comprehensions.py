20f7bc8f8951758a8ac4ca21641e25775c42b126
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-20 10:39:50 +0000

    23_04_Explain

--- part23/23_04_List_Comprehensions.py
+++ part23/23_04_List_Comprehensions.py
@@ -1,6 +1,8 @@
 
+#we have here one List
 things = [2, 5, 9]
 
+#we creat one value as Transformer expression  FORMULA list but here for value in things
 yourlist = [value * 2 for value in things]
 
 print(yourlist)
