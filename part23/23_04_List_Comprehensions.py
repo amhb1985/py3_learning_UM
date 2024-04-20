f8180eb4ad1d00c351f1bc0da5ba9ca9c5617943
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-20 10:44:18 +0000

    2nd_example_in_List_Comp

--- part23/23_04_List_Comprehensions.py
+++ part23/23_04_List_Comprehensions.py
@@ -1,9 +1,17 @@
-
+'''
 #we have here one List
 things = [2, 5, 9]
 
 #we creat one value as Transformer expression  FORMULA list but here for value in things
 yourlist = [value * 2 for value in things]
 
+yourlist = map (lambda value: value*2, things)
 print(yourlist)
 
+'''
+
+def keep_evens(nums):
+    new_list = [num for num in nums if num % 2 == 0]
+    return new_list
+
+print(keep_evens([3, 4, 6, 7, 0, 1]))
