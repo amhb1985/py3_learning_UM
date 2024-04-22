bd6c99a1a4041f677919f569bcc2b3cc3c9e8512
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-22 05:50:17 +0000

    23_05_zip_explain_append

--- part23/23_05_zip.py
+++ part23/23_05_zip.py
@@ -3,8 +3,10 @@
 #1. for the undersatnding the function of Zip in the py3 we can explain 
 # a similary append as i learnd before:
 
-#
+# we can see that we have 2 diffrent Lists and we can append both of them in list3:
 L1 = [3, 4, 5]
 L2 = [1, 2, 3]
 L3 = [x1 + x2 for (x1, x2) in list(zip(L1, L2))]
 print(L3)
+
+
