579f6e318949bfce7dbbcc11abab35bf2776f813
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-23 10:45:18 +0000

    24_08_unfortunatly_it_didnt_work

---
+++ part24/24_08_Dfine_Func_another_test.py
@@ -0,0 +1,44 @@
+
+
+#this is about : Defining a function to make repeated invocations
+# import statements for necessary Python modules
+#0 like another we must set first the Majule : request
+import requests
+
+#2 we define  one Fuction to our word:
+def get_rhymes(word):
+    #2.1 after that we set a varible as baseURL = "our free source as URL"
+    # I wanna change the API source
+    #baseurl = "https://api.datamuse.com/words"
+    baseurl = "hhttps://www.dreamfactory.com/?utm_source=capterra&utm_medium=directory&utm_campaign=api_management&utm_channel=capterra"
+
+    #2.2 set up an empty {Dictionary}
+    params_diction = {} # Set up an empty dictionary for query parameters
+
+    #2.3 set up two functions for our query paramthers: 
+    #   >>  1 ["rel_rhy"] = word   
+    #   >>  2  ["max"] = "3"
+    params_diction["rel_rhy"] = word
+    params_diction["max"] = "3" # get at most 3 results
+
+    # 3. set uo now one RESPOND var to  get requst 
+            #request.get(URL_var, param s= paramethers_var )
+    resp = requests.get(baseurl, params=params_diction)
+
+    # 4. set up another var to >> using the .json for our responing var
+    word_ds = resp.json()    # return the top three words
+
+    # 5. at end we are returning >>
+    #5.1 >> [ d['word in 18 lines'] for d in word_ds VAR in 26 line ]
+    # if we not set this return we can see whole Dictionary after running the code
+    #first try to see 
+    #return [d['word'] for d in word_ds]
+
+    #5.2 >> rep.json()
+    return resp.json()
+ # Return a python object (a list of dictionaries in this case)
+
+#print(get_rhymes("book"))
+#print(get_rhymes("dash"))
+
+
