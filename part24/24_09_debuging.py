38c2618e86a59ac0e6ee9511665cbfa748eb6ff8
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-24 11:52:13 +0000

    24_09_debugging_after_running_with_mistake_Index

--- part24/24_09_debuging.py
+++ part24/24_09_debuging.py
@@ -4,7 +4,7 @@
 import requests
 
 #we set one varible to get requests from one URl as our source
-r = requests.get("https://api.datamuse.com/words", params ={"rel_rhy": "funny"})
+r = requests.get("https://api.datamuse.com/words", params ={"relrhy": "funny"})
 print(r.text)
 print(r.url)
 
