c9088b7823695c532cba65dce295e9806ee7c6cc
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-24 11:53:51 +0000

    24_09_debugging_mistake_explain

--- part24/24_09_debuging.py
+++ part24/24_09_debuging.py
@@ -5,6 +5,7 @@ import requests
 
 #we set one varible to get requests from one URl as our source
 r = requests.get("https://api.datamuse.com/words", params ={"relrhy": "funny"})
+#the best way to debugging the code is test the URL after the error
 print(r.text)
 print(r.url)
 
