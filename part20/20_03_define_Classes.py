cd46b8d197d2dc4bc664bdd449dcd2e8a7732622
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-26 14:18:27 +0000

    23_02_class_point_x

--- part20/20_03_define_Classes.py
+++ part20/20_03_define_Classes.py
@@ -9,11 +9,22 @@ point1 = Point()
 point2 = Point()
 
 # after running the print we can see that we have two diffrent Object in our Class
-print(point1)
-print(point2)
+#print(point1)
+#print(point2)
 
 # now we can check the result of class
 # we can see that point1 is NOT equal pont2 !
+'''
 print(point1 == point2)
 print(point1 != point2)
+'''
+#so we can set a formula for our points: with " .x = "
+
+point1.x = 5 
+point2.x = 10 
+print(point1.x)
+print(point2.x)
+
+
+
 
