b3ff1653739e2a078eb616110b6d230f3db6b063
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-26 14:10:57 +0000

    23_03_Class_tseting_the_Result_2_point

--- part20/20_03_define_Classes.py
+++ part20/20_03_define_Classes.py
@@ -12,4 +12,8 @@ point2 = Point()
 print(point1)
 print(point2)
 
+# now we can check the result of class
+# we can see that point1 is NOT equal pont2 !
+print(point1 == point2)
+print(point1 != point2)
 
