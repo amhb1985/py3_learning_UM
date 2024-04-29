67bc8b5f8f3c98e1e2a39d169f8d2b8df961c9f9
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-29 09:35:17 +0000

    20_05_adding_in_class

---
+++ part20/20_05_adding_other_methods_at_a_class.py
@@ -0,0 +1,13 @@
+#adding other Methods in a Class
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def getX(slef):
+        return self.x
+    def getY(slef):
+        return self.y
+    
+
+point1 = Point(10, 100)
