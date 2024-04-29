a24f7eabfd77020500c19011474c0726621a7059
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-04-29 11:35:10 +0000

    20_07_use_def_str

--- part20/20_07_Converting_an_Object_2_String.py
+++ part20/20_07_Converting_an_Object_2_String.py
@@ -17,6 +17,12 @@ class Point:
     def distanceFromOrigin(self):
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
+    #for the solve of it we must >> define a def __str__ (self) and exactly return it
+    def __str__(self):
+        #then we can see the 'Point'
+        return('Point 123')
+
+
 #so after the running the test we will see that just one message :<__main__.Point object at 0x7f5549b8bfd0> 
 p = Point(7,6)
 print(p)
