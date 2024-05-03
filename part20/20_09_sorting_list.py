8bdb5f77eb8e54e9b1d129456d78c463e7883573
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-03 09:13:02 +0000

    20_09_sorting_list_explain01

--- part20/20_09_sorting_list.py
+++ part20/20_09_sorting_list.py
@@ -1,9 +1,8 @@
-
-
 #so now we test anotother type to sorting a list:
-
+#1. first of all we have a list for some Fruits like this
 L = ["Cherry", "Apple", "Blueberry"]
 
+#now we try to sort the list with key as len >> how long will be one word
 print(sorted(L, key=len))
 #alternative form using lambda, if you find that easier to understand
 print(sorted(L, key= lambda x: len(x)))
