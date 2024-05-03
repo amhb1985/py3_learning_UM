87b87f54da4ce7781444863b098b0d92ceb69d32
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-03 13:34:23 +0000

    20_final_test_week_1_q_3

--- part20/20_15_final_test_week1.py
+++ part20/20_15_final_test_week1.py
@@ -20,7 +20,7 @@ testTwo=Bike("purple",25.0)
 #quantity of apples. The constructor should initialize two instance variables: apple_color and apple_quantity. Write a class method called 
 #increase that increases the quantity by 1 each time it is invoked. You should also write a __str__ method for this class that returns a string of the format: "A basket of [quantity goes here] [color goes here] apples." e.g. "A basket of 4 red apples." or "A basket of
 # 50 blue apples." (Writing some test code that creates instances and assigns values to variables may help you solve this problem!)
-
+'''
 class AppleBasket:
     def __init__(self,color,quant):
         self.apple_color=color
@@ -36,4 +36,23 @@ class AppleBasket:
 val1=AppleBasket('red',8)
 print(val1)
 print(val1.increase())
-     
+    ''' 
+#q3:Define a class called BankAccount that accepts the name you want associated with your bank account in a string, 
+#and an integer that represents the amount of money in the account. The constructor should initialize two instance variables from those inputs: name and amt. Add a string method so that when you print an instance of BankAccount, you see "Your account, [name goes here], has [start_amt goes here] dollars." 
+#Create an instance of this class with "Bob" as the name and 100 as the amount. Save this to the variable t1.
+
+
+class BankAccount:
+    def __init__(self,name,amt):
+        self.name=name
+        self.amt=amt
+        
+    def __str__(self):
+        return "Your account, {}, has {} dollars.".format(self.name,self.amt)
+    
+t1=BankAccount("Bob",100)
+#t2=BankAccount("Bob",2000)
+
+print(t1)
+#print(t1)
+
