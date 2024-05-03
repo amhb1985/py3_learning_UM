c828c8508c4928a515539ff58f9912a8757f8b22
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-03 09:16:41 +0000

    20_09_example_02

--- part20/20_09_sorting_list.py
+++ part20/20_09_sorting_list.py
@@ -1,3 +1,5 @@
+'''
+#example 1: 
 #so now we test anotother type to sorting a list:
 #1. first of all we have a list for some Fruits like this
 L = ["Cherry", "Apple", "Blueberry"]
@@ -6,3 +8,16 @@ L = ["Cherry", "Apple", "Blueberry"]
 print(sorted(L, key=len))
 #alternative form using lambda, if you find that easier to understand
 print(sorted(L, key= lambda x: len(x)))
+'''
+
+#example 2 :
+# we have now another example for sorting the list with using the price of them:
+
+class Fruit():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+L = [Fruit("Cherry", 10), Fruit("Apple", 5), Fruit("Blueberry", 20)]
+for f in sorted(L, key=lambda x: x.price):
+    print(f.name)
