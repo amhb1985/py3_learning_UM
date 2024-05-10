9a5f28e523e5c32eb3cb1a4decd382e0d539e9b3
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-10 09:16:23 +0000

    22_01_set_current_year_and _explain

--- part22/22_02_inherit01.py
+++ part22/22_02_inherit01.py
@@ -1,9 +1,16 @@
 #this is aabout the example in inherit:
+# in thsi example we try calculte the age with the current year as 2024
 
+#1 first of all we must set one class like this:
 
-#0. first of all we must set one class like this:
-
+current_year = 2024
 class Person:
-    #0.1 then we define inint (naturally with self, name, and the year of birth)
-    def __init__(self, name, year):
-        
+    #1.1 then we define inint (naturally with self, name, and the year of birth)
+    def __init__(self, name, year_born):
+        self.name = name
+        self.year_born = year_born
+
+    #1.2 so we need define the getAge and erduce this from Current Year
+    def getAge(self):
+        return current_year - self.year_born
+
