89cd6c2ec2889e275024f9aa5f350990c491157f
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-10 09:10:00 +0000

    22_01_test_in_inherite

---
+++ part22/22_02_inherit01.py
@@ -0,0 +1,9 @@
+#this is aabout the example in inherit:
+
+
+#0. first of all we must set one class like this:
+
+class Person:
+    #0.1 then we define inint (naturally with self, name, and the year of birth)
+    def __init__(self, name, year):
+        
