02b011b2bcd6f4c969eb40bb4796d04a07146e0c
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-11 10:53:32 +0000

    22_03_explain_and_edit_paperBooks

--- part22/22_03_Overriding.py
+++ part22/22_03_Overriding.py
@@ -9,7 +9,20 @@ class Book():
     #1.2 define Str methode 
     def __str__(self):
         return '{}, by {}'.format(self.title, self.author)
-    
+#2. we can change the 2 sub-class to e e-books and papaer books:
+#Notice : inside of() we must set our main Class. >>> here is Book
+class PaperBook(Book):
+    # we define  the methode init 
+    # 2.2 we must our main Class (here Book) join with init >>> IT IS NOT SELF !!!
+    def __init__(self, author, numPages):
+        Book.__init__(self, title, author)
+        # we set self for Number of pages
+        self.numPages = numPages
+
+
 #test the book:
-myBook = Book('the Odysse', 'Homer')
-print(myBook)
+#myBook = Book('the Odysse', 'Homer')
+#print(myBook)
+
+
+
