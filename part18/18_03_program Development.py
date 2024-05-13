34ea407915488ada02658e198a46e22523f41b64
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-13 10:54:48 +0000

    18_03_Pro_Dev_02

--- part18/18_03_program Development.py
+++ part18/18_03_program Development.py
@@ -1,8 +1,9 @@
 
-
+#again like another example test it didnt working
 import test
 def distance(x1, y1,x2, y2):
-    return None
+    return 0
 
 test.testEqual(distance(1,2,1,2), 0 )
-
+test.testEqual(distance(1,2,4,6),5 )
+test.testEqual(distance(0,0, 1,1), 2**0.5)
