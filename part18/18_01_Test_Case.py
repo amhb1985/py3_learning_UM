d3915536923beb8aa363b5e0bb4ffe9275b36882
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-13 05:57:24 +0000

    18_01_test_equal_

---
+++ part18/18_01_Test_Case.py
@@ -0,0 +1,14 @@
+# as the first code we use this code:
+
+#for example define a Function for squre of X:
+def square(x):
+    return x * x
+
+#01 first we must import it
+import test
+#then we print our result:
+print('testing squre function')
+
+#03. we must testning  testEquel >> ourFunction (the X), and the Answere
+#>> when the answere will true , then comming TRUE
+test.testEqual(square(10),100)
