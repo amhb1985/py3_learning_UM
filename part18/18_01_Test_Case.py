22edcaaffbd68e464d2d43baec3c9f984e6da2ce
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-13 06:19:58 +0000

    18_01_tset_equel_02

--- part18/18_01_Test_Case.py
+++ part18/18_01_Test_Case.py
@@ -10,5 +10,11 @@ import test
 print('testing squre function')
 
 #03. we must testning  testEquel >> ourFunction (the X), and the Answere
-#>> when the answere will true , then comming TRUE
+#>> when the answere will true , then comming TRUE as 'Pass'!! in another hand comming the 'TEST Failed'
+#>> so for the first Try unfortionatly we can not un it, may bee we must change the testEqual to new Version
 test.testEqual(square(10),100)
+#testing with anothers:
+test.testEqual(square(3),9)
+test.testEqual(square(-4),16)
+test.testEqual(square(1.5),2.25)
+test.testEqual((0),0)
