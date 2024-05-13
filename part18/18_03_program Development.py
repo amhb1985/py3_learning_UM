41e4d07d62179398327877ec13bdf44757de4d63
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-13 11:00:12 +0000

    18_03_Prog_Dev_with_test

--- part18/18_03_program Development.py
+++ part18/18_03_program Development.py
@@ -1,8 +1,11 @@
-
-#again like another example test it didnt working
 import test
 def distance(x1, y1,x2, y2):
-    return 0
+    #so we can correct fo ozur example to this one:
+    dx = x2 - x1
+    dy = y2 - y1
+    dsquared = dx**2 + dy**2
+    result = dsquared**0.5
+    return result
 
 test.testEqual(distance(1,2,1,2), 0 )
 test.testEqual(distance(1,2,4,6),5 )
