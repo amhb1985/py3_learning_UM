c57dbdb2079b2a8a1c963d0ca77547a5a8a54d1b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-16 10:09:30 +0000

    19_02_set_3_mistakes

--- part19/19_02_try_except.py
+++ part19/19_02_try_except.py
@@ -1,3 +1,16 @@
 #this is about some developed try/except Methode inside of our code
 #as we can learn further we have 3 kind of Error .
 # now we try to set 3 diffrent except for our first code:
+
+items = ['a', 'b','c']
+
+try:
+    #we set 3 diffrent mistake in our code:
+    myvar= a
+    x = 10/0
+    third = items[2]
+    print("a")
+except:
+    print("Something went Wrong!")
+    third = False
+print("I wanna this to run")
