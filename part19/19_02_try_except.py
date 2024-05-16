a0ab79aef51f3156340645d4a26b7486ed35d763
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-16 10:14:59 +0000

    19_02_except_IndexError

--- part19/19_02_try_except.py
+++ part19/19_02_try_except.py
@@ -2,15 +2,23 @@
 #as we can learn further we have 3 kind of Error .
 # now we try to set 3 diffrent except for our first code:
 
-items = ['a', 'b','c']
+items = ['a', 'b']
 
 try:
     #we set 3 diffrent mistake in our code:
-    myvar= a
-    x = 10/0
+    #myvar= a
+    #x = 10/0
     third = items[2]
-    print("a")
-except:
-    print("Something went Wrong!")
-    third = False
-print("I wanna this to run")
+    
+except ZeroDivisionError:
+    print("You can not divid by Zero!")
+
+except IndexError:
+    print("Index out of bounds")
+
+except NameError:
+    print("Tried to fetch a name that did not Exist!")
+
+
+
+print("this does RUN!")
