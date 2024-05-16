e28957aa04a829d72c6f2f528c7aefac4904d4ee
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-16 09:24:03 +0000

    19_02_explain_3_type_of_Inex_error

--- part19/19_02_exception.py
+++ part19/19_02_exception.py
@@ -6,3 +6,8 @@ third = items[2]
 #after running the code we can see as below:
 #IndexError: list index out of range
 #its mean that we have error because we had just 2 item in our list NOT 3!
+
+#as we can see we have three Error when we have mistake:
+#1. Syntadic >> like a () or []
+#2.Runtime >>. like a item out of our List
+#3.Semantic >>
