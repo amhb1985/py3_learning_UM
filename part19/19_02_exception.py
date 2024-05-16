413e68376c737e7c84cb53bdc142d683778c99c0
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-16 09:20:00 +0000

    19_02_see_another_SyntexError_out_Of_Range

--- part19/19_02_exception.py
+++ part19/19_02_exception.py
@@ -1,5 +1,8 @@
 
 #so in this py code wewill see the Index Error after running the code:
 #
-items = ['a', 'b'
-third = items[2
+items = ['a', 'b']
+third = items[2]
+#after running the code we can see as below:
+#IndexError: list index out of range
+#its mean that we have error because we had just 2 item in our list NOT 3!
