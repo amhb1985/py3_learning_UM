a1a821c9f00f26b631c9a2d0e3d2a5b2e0ec6bed
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-22 18:05:27 +0000

    chnage_alittle

--- part19/19_02_try_except.py
+++ part19/19_02_try_except.py
@@ -1,7 +1,7 @@
 #this is about some developed try/except Methode inside of our code
 #as we can learn further we have 3 kind of Error .
 # now we try to set 3 diffrent except for our first code:
-
+# ok againtest it
 items = ['a', 'b']
 
 try:
