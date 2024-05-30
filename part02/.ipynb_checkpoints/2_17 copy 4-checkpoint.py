8ca1d4f79ab3781c909ae3f0415ff09ae6723958
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-05-30 06:22:13 +0000

    02_17_04_test
    definition

---
+++ part02/.ipynb_checkpoints/2_17 copy 4-checkpoint.py
@@ -0,0 +1,17 @@
+
+'''
+challenge4
+There is a function we are providing in for you in this problem called square. 
+It takes one integer and returns the square of that integer value. 
+Write code to assign a variable called xyz the value 5*5 (five squared). 
+Use the square function, rather than just multiplying with *
+'''
+# Assuming you have a function named square
+def square(x):
+    return x ** 2
+
+# Assign the variable xyz the value of 5 squared using the square function
+xyz = square(6)
+
+# Print the result
+print(xyz)
