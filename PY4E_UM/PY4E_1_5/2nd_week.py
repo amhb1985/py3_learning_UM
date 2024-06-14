12de37a66312cbe81eb8a9c9cbe4eb190f5016e3
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-14 11:04:50 +0000

    move_old_data_to_new_folder

---
+++ PY4E_UM/PY4E_1_5/2nd_week.py
@@ -0,0 +1,4 @@
+
+#this is the py3 learning under the PY4E Python for every one Course in Uni. Michigan with Dr. Chuck
+
+# we use this website to run Py3 scripts: https://www.py4e.com/tools/pythonauto/?PHPSESSID=a6a89886f81243569354192792f28e33  and test the answers.
