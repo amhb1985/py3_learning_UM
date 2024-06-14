db4386bbe4f64f83a64d9ea1a2cc505cefdfd67a
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-14 11:30:35 +0000

    checking_the_install_and_run_py_in_Terminal_in_Win

--- PY4E_UM/PY4E_1_5/2nd_week.py
+++ PY4E_UM/PY4E_1_5/2nd_week.py
@@ -4,5 +4,16 @@
 # we use this website to run Py3 scripts: https://www.py4e.com/tools/pythonauto/?PHPSESSID=a6a89886f81243569354192792f28e33  and test the answers.
 
 print('play')
+# so frist of all we must download the python progrom for win or fo the mac
 
+'''
+#windows 
+for checking the install we can use this commend >>
+python --version
+then we can see the version of the our installed Python
 
+#after that installing we can run the code >>
+python 
+ inside of the Terminal in win to run it
+
+'''
