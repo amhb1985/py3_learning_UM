f995c1dfd916a32310c6143161527013e7936aa4
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-06-17 10:03:12 +0000

    INFO_comparision_Operators

--- PY4E_UM/PY4E_1_5/5th_week.py
+++ PY4E_UM/PY4E_1_5/5th_week.py
@@ -2,10 +2,20 @@
 #thid is about the 5th week learning
 
 #conditional Steps:
-
+# if explain about the conditional situations
 x = 30
 if x < 10 : 
     print("smaller")
 if x > 20 : 
     print("Bigger")
 print("Finish!")
+'''
+# and the now we speak about the Comparision Operators:
+<   less than
+<=  less than or Equal TimeoutError
+==   Equal to 
+>=  grater than or Equal 
+>   greater than
+!=  Not Equal 
+'''
+
