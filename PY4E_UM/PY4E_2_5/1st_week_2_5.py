9ddded8842a0b7d80f984aaa7fcecb030cfbdc9f
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-07-02 12:51:53 +0000

    test_looking_inside_String_test

--- PY4E_UM/PY4E_2_5/1st_week_2_5.py
+++ PY4E_UM/PY4E_2_5/1st_week_2_5.py
@@ -4,3 +4,21 @@
 # so we can do again for this week 
 
 # this one actuallly is chapter 6 and about String data type
+# we r speaking now about diffrent Issue:
+'''
+1. String Data Type
+
+2. Reading and Converting
+its like some Input
+as string or Integer
+
+3. Looking Inside Strings
+like a charachter or each word inside of a varible for example 'banana'
+'''
+fruit = 'banana'
+letter = fruit[1]
+print(letter)
+
+
+
+
