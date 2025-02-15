1a328db6ac8726c9638c32a3e0e9de73260b3e79
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2025-02-15 10:21:40 +0000

    mPy_another

---
+++ PY_Casio/1_4_2_tset_andere.py
@@ -0,0 +1,10 @@
+
+import casioplot
+
+# Einfache Linie von (0,0) nach (127,63) zeichnen
+for x in range(128):  # Bildschirmbreite: 128 Pixel
+    y = int(x * 0.5)  # Linie mit Steigung 0.5
+    casioplot.set_pixel(x, y, (0, 0, 0))  # Pixel setzen (schwarz)
+
+# Bildschirm aktualisieren
+casioplot.show()
