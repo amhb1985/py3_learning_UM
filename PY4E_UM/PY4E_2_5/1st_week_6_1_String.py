289f71795cb0426d9ecefca0911887c1c12df7bb
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2025-02-26 04:11:34 +0000

    PY4E_w06_lenght

--- PY4E_UM/PY4E_2_5/1st_week_6_1_String.py
+++ PY4E_UM/PY4E_2_5/1st_week_6_1_String.py
@@ -2,8 +2,14 @@
 
 # so again we can test aand learn with Py:
 # String:
-
+'''
 zot = 'abc'
 print(zot)
 print(zot[0:2])
+'''
+'''# String Have Length
+fruit = 'banana'
+print(len(fruit))
+'''
 
+#len() is a Function!
