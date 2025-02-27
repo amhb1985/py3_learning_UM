57bca7e868ae39cf20e9b7eb84d826056462e28b
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2025-02-27 05:41:50 +0000

    yp4e_w_5_Counting_iin_with _loop

--- PY4E_UM/PY4E_2_5/1st_week_6_1_String.py
+++ PY4E_UM/PY4E_2_5/1st_week_6_1_String.py
@@ -19,12 +19,23 @@ for letter in obst:
     print(letter)
 '''
 #2. Dies ist gleich Oben aber mit mehr Details.
+'''
 obst2 = " APFEL " 
 index = 0 
 while index < len (obst2):
     letter = obst2[index]
     print(letter)
-    index = index + 2
+    index = index + 2'''
+#3. Looping and Counting
+# mit eine  'for in ' kann die Alpabet (d.h. string) zählen kann
+Worter = 'Bananen'
+count = 0 
+for letter in Worter:
+    if letter == 'a':
+        count = count + 1
+    
+print(count)
+
 
 
 
