5feb07d194b12e9761a8551913c439552a710958
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2025-02-27 05:34:23 +0000

    py4e_w_04

--- PY4E_UM/PY4E_2_5/1st_week_6_1_String.py
+++ PY4E_UM/PY4E_2_5/1st_week_6_1_String.py
@@ -13,17 +13,18 @@ fruit = 'banana'
 print(len(fruit))
 '''
 # 2. nächst: Looping Through String
+'''
 obst = "apfel"
 for letter in obst:
     print(letter)
-
+'''
 #2. Dies ist gleich Oben aber mit mehr Details.
 obst2 = " APFEL " 
 index = 0 
 while index < len (obst2):
     letter = obst2[index]
     print(letter)
-    index = index + 1 
+    index = index + 2
 
 
 
