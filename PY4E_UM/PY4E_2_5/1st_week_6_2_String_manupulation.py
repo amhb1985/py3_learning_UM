24cf81dfe00e0ab6d1dfa1ed9a47808bb8453d7d
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2025-03-02 06:43:21 +0000

    py4e_04_String_comparison

--- PY4E_UM/PY4E_2_5/1st_week_6_2_String_manupulation.py
+++ PY4E_UM/PY4E_2_5/1st_week_6_2_String_manupulation.py
@@ -7,4 +7,15 @@ print(c)
 '''
 #
 
+ 02_String _comparison#
+if Wort == "Apfel" :
+    print("alles gut, Apfel")
+if Wort < 'Apfel' :
+   print("Dein Wort,+ Wort + 'kommt bevor dem Apfel.' ")
+elif Wort >'Apfel':
+   print("Dein Wort,+ Wort + 'kommt nach dem Apfel.' ")
+else:
+    print("alles gut, Apfel")
+   
+
 
