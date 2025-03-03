3f26a77451203a73458e14b11386359426a56b4a
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2025-03-03 04:22:58 +0000

    mPy_02_a

---
+++ PY_Casio/Pytha_alt_a_b_c.py
@@ -0,0 +1,16 @@
+
+#diese sind erste versuch für mPy in der TR:
+
+# a: AnKath:
+import math
+a = float(input("enter a = "))
+"""b = float(input("enter b = "))"""
+c = float(input("c muss grosser als b = "))
+d =(c**2-a**2)
+b=math.sqrt(d)
+"""h = sqrt((c**2)-(b**2))
+print("p ist = ")
+print(p)"""
+print("b ist=")
+print(b)
+
